e8a59b301e8a983ad5c63ef477f492c7a1c1c222
459633371 <459633371@qq.com>
2020-05-23 03:11:27 +0800

    init repo

---
+++ src/main/java/com/zhaoming/zhaoming/ZhaomingApplication.java
@@ -0,0 +1,14 @@
+package com.zhaoming.zhaoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhaomingApplication {
+
+    public static void main(String[] args) {
+       // system.out.print("sdfs")
+        SpringApplication.run(ZhaomingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhaoming/zhaoming/ZhaomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoming.zhaoming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhaomingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
